73bfb6f078f141c2b958e31e14859c20b6b2a20c
Ricky Soto <rickysotosanchezz@gmail.com>
2020-03-29 17:44:44 -0400

    add text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn new things
+          Learn ricky
         </a>
       </header>
     </div>
