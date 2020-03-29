ea9ac87d96d65b96eeb74769050b06ddec88b89f
Ricky Soto <rickysoto@Rickys-MacBook-Air.local>
2020-03-29 17:37:37 -0400

    add text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Klk palomon
+          Learn new things
         </a>
       </header>
     </div>
